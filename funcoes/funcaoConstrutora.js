4c6a650cda07cce16201bba76f3b0d62266e23df
Asael Silva <asael.guitar_g3@hotmail.com>
2019-10-07 09:50:58 -0300

    Curso-Web-Moderno-Com-JavaScript- Funções- funções construtoras.

--- funcoes/funcaoConstrutora.js
+++ funcoes/funcaoConstrutora.js
@@ -1,9 +1,25 @@
 function carro (velocidadeMaxima = 200, delta = 5) {
     // atribto privado
-    let velocidadeMaxima = 0
+    let velocidadeAtual = 0
 
     // metodo publico
     this.acelerar = function () {
-        
+        if (velocidadeAtual + delta <= velocidadeMaxima) {
+            velocidadeAtual += delta
+        }else{
+            velocidadeAtual = velocidadeMaxima
+        }
     }
+
+    // metodo publico
+    this.getVelocidadeAtual = function () {
+    return getVelocidadeAtual
+    }
+
+    const uno = new carro
+    uno.acelerar()
+    console.log(uno.getVelocidadeAtual())
+
+    
+
 }
