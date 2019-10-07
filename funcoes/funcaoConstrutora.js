a4a604ae1a6a05499544ba4d05ab4f508a369d62
Asael Silva <asael.guitar_g3@hotmail.com>
2019-10-07 09:35:27 -0300

    Curso-Web-Moderno-Com-JavaScript- Funções- funções construtoras.

---
+++ funcoes/funcaoConstrutora.js
@@ -0,0 +1,9 @@
+function carro (velocidadeMaxima = 200, delta = 5) {
+    // atribto privado
+    let velocidadeMaxima = 0
+
+    // metodo publico
+    this.acelerar = function () {
+        
+    }
+}
