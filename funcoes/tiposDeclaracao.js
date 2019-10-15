36f37ec5b95385a2670896e1fd38674b4fe47bed
Asael Silva <asael.guitar_g3@hotmail.com>
2019-10-15 10:15:14 -0300

    Curso-Web-Moderno-Com-JavaScript- Funções- Tipos de declaração.

---
+++ funcoes/tiposDeclaracao.js
@@ -0,0 +1,23 @@
+console.log(soma(5, 5));
+
+
+//function declaration
+function soma(x, y) {
+    return x + y
+}
+
+
+
+//function expression
+const sub = function (x, y) {
+    return x - y 
+}
+console.log(sub(10, 5));
+
+
+
+//name function expression
+const mult = function mult (x, y) {
+    return x * y 
+}
+console.log(mult(10, 10));
