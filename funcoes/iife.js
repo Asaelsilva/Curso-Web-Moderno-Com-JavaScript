a9ea85cdcb5a0854ca197fd12fdef70ff966d622
Asael Silva <asael.guitar_g3@hotmail.com>
2019-10-22 12:46:56 -0300

    Curso-Web-Moderno-Com-JavaScript- Funções- IIFE

---
+++ funcoes/iife.js
@@ -0,0 +1,7 @@
+// IIFE -> Immediately Invoked Function Expression = Expressão de função invocada imediatamente
+
+(function () {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()
+
