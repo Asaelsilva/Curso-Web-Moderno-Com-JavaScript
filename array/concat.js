d562a4eded39417876c6462b1ea93901af0f4beb
Asael Silva <asael.guitar_g3@hotmail.com>
2019-11-22 13:08:26 -0300

    Concat e flatMap

---
+++ array/concat.js
@@ -0,0 +1,16 @@
+const filhas = ['Adna', 'Acsa']
+const filhos = ['Jonatas', 'Asael']
+const todos = filhas.concat(filhos, 'Natanael', 'Sara')
+
+console.log(filhas, filhos)
+console.log(todos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
+
+
+
+
+
+
+
+
